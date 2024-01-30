c0c49f2a6a136443aeab37d67d8f94aa3f7a4a7f
JoseIgnacioTapia <josignaciot@gmail.com>
2024-01-30 18:29:09 -0300

    Sign In - Sign out and Check Auth Status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
 import { Button } from "@nextui-org/react";
 import * as actions from "@/actions";
 import { auth } from "@/auth";
+import Profile from "@/components/profile";
 
 export default async function Home() {
   const session = await auth();
 
   return (
     <div>
-      <form action={actions.signIn}>
+      <form action={actions.signIn} className="m-4">
         <Button type="submit">Sign In</Button>
       </form>
 
-      <form action={actions.signOut}>
+      <form action={actions.signOut} className="m-4">
         <Button type="submit">Sign Out</Button>
       </form>
 
@@ -20,6 +21,8 @@ export default async function Home() {
       ) : (
         <div>Signed Out</div>
       )}
+
+      <Profile />
     </div>
   );
 }
